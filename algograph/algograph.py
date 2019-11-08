c7df58ed9c7958f2feb33b3d61ef43cf81b30fa2
Genadij Razdorov <genadijrazdorov@gmail.com>
2019-11-08 14:07:24 +0100

    Added basic algo2dot implementation.Fixed #3

--- algograph/algograph.py
+++ algograph/algograph.py
@@ -1,3 +1,19 @@
 
+TO = ' -> '
+
+def iterlines(algorithm):
+    for line in algorithm.splitlines():
+        yield from line.split('; ')
+
 def algo2dot(algorithm):
-    pass
+    dot = ['digraph {']
+    lines = iterlines(algorithm)
+    dot.append('\t' + next(lines))
+
+    for line in lines:
+        dot[-1] += TO + line
+        dot.append('\t' + line)
+
+    del dot[-1]
+    dot.append('}')
+    return '\n'.join(dot)
