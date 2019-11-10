959aa501acf1bdb76536601cef7081b3b03b3a31
Genadij Razdorov <genadijrazdorov@gmail.com>
2019-11-10 18:55:25 +0100

    Added strings as constants

--- algograph/algograph.py
+++ algograph/algograph.py
@@ -1,19 +1,26 @@
 
-TO = ' -> '
+TO = '->'
+SPACE = ' '
+INDENT = '\t'
+
+DIGRAPH_BEGIN = 'digraph {'
+DIGRAPH_END = '}'
+
 
 def iterlines(algorithm):
     for line in algorithm.splitlines():
         yield from line.split('; ')
 
+
 def algo2dot(algorithm):
-    dot = ['digraph {']
+    dot = [DIGRAPH_BEGIN]
     lines = iterlines(algorithm)
     dot.append('\t' + next(lines))
 
     for line in lines:
-        dot[-1] += TO + line
+        dot[-1] += SPACE + TO + SPACE + line
         dot.append('\t' + line)
 
     del dot[-1]
-    dot.append('}')
+    dot.append(DIGRAPH_END)
     return '\n'.join(dot)
