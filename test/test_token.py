0038d86d07aaa2b8771f563ab3648d9b4e16a002
Genadij Razdorov <genadijrazdorov@gmail.com>
2019-11-27 14:58:48 +0100

    Changed names of ID/IF tokens in test_token

--- test/test_token.py
+++ test/test_token.py
@@ -3,27 +3,27 @@ from algograph.token import TOKEN, _VTOKEN
 import pytest
 
 
-class IF(TOKEN):
+class TTOKEN(TOKEN):
     regex = r'\bif\b'
 
 
-class ID(_VTOKEN):
+class T_VTOKEN(_VTOKEN):
     regex = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
 
 class TestToken:
     def test__eq__(self):
-        assert IF() == IF()
-        assert IF() != TOKEN()
+        assert TTOKEN() == TTOKEN()
+        assert TTOKEN() != TOKEN()
 
     def test__repr__(self):
-        assert repr(IF()) == 'IF()'
+        assert repr(TTOKEN()) == 'TTOKEN()'
 
 
 class Test_VToken:
     def test__eq__(self):
-        assert ID('proba') == ID('proba')
-        assert ID('proba') != ID('p')
+        assert T_VTOKEN('proba') == T_VTOKEN('proba')
+        assert T_VTOKEN('proba') != T_VTOKEN('p')
 
     def test__repr__(self):
-        assert repr(ID('proba')) == "ID('proba')"
+        assert repr(T_VTOKEN('proba')) == "T_VTOKEN('proba')"
