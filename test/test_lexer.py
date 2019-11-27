cdfc6fa47e3276fc757ffc2e241f3b0462b7effc
Genadij Razdorov <genadijrazdorov@gmail.com>
2019-11-27 12:37:03 +0100

    Added tests for Lexer

--- test/test_lexer.py
+++ test/test_lexer.py
@@ -1,31 +1,94 @@
-from algograph.lexer import Lexer, ID, LITERAL
+from algograph.lexer import *
 
 import pytest
 
 
+def tokenize(doc):
+    doc = doc.splitlines()
+    if not doc[0].strip():
+        doc = doc[1:]
+    doc = '\n'.join(line.lstrip()[2:] for line in doc)
+    return list(Lexer(doc).tokenize())
+
+
 class TestLexer:
     def test_start_end(self):
-        assert list(Lexer('start; end').tokenize()) == [
+        assert tokenize('| start; end') == [
             ID('start'),
             LITERAL(';'),
-            ID('end')
+            ID('end'),
+            NEWLINE()
         ]
 
-    ## def test_keywords(self):
-    ##     assert list(tokenizer('\n'.join(line.strip()[2:] for line in '''
-    ##         | if question:
-    ##         |     yes
-    ##         | else:
-    ##         |     no
-    ##         | middle
-    ##     '''.splitlines()[1:-1]))) == [
-    ##         KEYWORD('if'),
-    ##         ID('question'),
-    ##         PUNCT(':'),
-    ##         ID('yes'),
-    ##         KEYWORD('else'),
-    ##         PUNCT(':'),
-    ##         ID('no'),
-    ##         ID('middle')
-    ##     ]
+    def test_unknown_token(self):
+        with pytest.raises(ValueError) as err:
+            tokenize('| start; 9 end')
 
+        assert "Unknown token '9' at line 0" in str(err.value)
+
+    def test_newline(self):
+        assert tokenize('| start\n|\n| end') == [
+            ID('start'),
+            NEWLINE(),
+            ID('end'),
+            NEWLINE()
+        ]
+
+    def test_indent_dedent(self):
+        assert tokenize('''
+                | if q:
+                |   y
+                | end
+            ''') == [
+                IF(),
+                ID('q'),
+                LITERAL(':'),
+                NEWLINE(),
+                INDENT(2),
+                ID('y'),
+                NEWLINE(),
+                DEDENT(0),
+                ID('end'),
+                NEWLINE()
+        ]
+
+    def test_keywords(self):
+        assert tokenize('''
+                | if q is a:
+                |   one
+                | elif q is b:
+                |   two
+                | else:
+                |   three
+                | end
+            ''') == [
+                IF(),
+                ID('q'),
+                IS(),
+                ID('a'),
+                LITERAL(':'),
+                NEWLINE(),
+                INDENT(2),
+                ID('one'),
+                NEWLINE(),
+                DEDENT(0),
+                ELIF(),
+                ID('q'),
+                IS(),
+                ID('b'),
+                LITERAL(':'),
+                NEWLINE(),
+                INDENT(2),
+                ID('two'),
+                NEWLINE(),
+                DEDENT(0),
+                ELSE(),
+                LITERAL(':'),
+                NEWLINE(),
+                INDENT(2),
+                ID('three'),
+                NEWLINE(),
+                DEDENT(0),
+                ID('end'),
+                NEWLINE()
+        ]
