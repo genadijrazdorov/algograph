50f9a9d104260cc825918f10a55837157db8bf95
Genadij Razdorov <genadijrazdorov@gmail.com>
2019-11-28 09:26:40 +0100

    Changed IF/ID class to fixture in tests.

--- test/test_token.py
+++ test/test_token.py
@@ -3,27 +3,35 @@ from algograph.token import TOKEN, _VTOKEN
 import pytest
 
 
-class TTOKEN(TOKEN):
-    regex = r'\bif\b'
+@pytest.fixture
+def if_():
+    class IF(TOKEN):
+        regex = r'\bif\b'
+
+    return IF
 
 
-class T_VTOKEN(_VTOKEN):
-    regex = r'[a-zA-Z_][a-zA-Z0-9_]*'
+@pytest.fixture
+def id_():
+    class ID(_VTOKEN):
+        regex = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
+    return ID
 
 
 class TestToken:
-    def test__eq__(self):
-        assert TTOKEN() == TTOKEN()
-        assert TTOKEN() != TOKEN()
+    def test__eq__(self, if_):
+        assert if_() == if_()
+        assert if_() != TOKEN()
 
-    def test__repr__(self):
-        assert repr(TTOKEN()) == 'TTOKEN()'
+    def test__repr__(self, if_):
+        assert repr(if_()) == 'IF()'
 
 
 class Test_VToken:
-    def test__eq__(self):
-        assert T_VTOKEN('proba') == T_VTOKEN('proba')
-        assert T_VTOKEN('proba') != T_VTOKEN('p')
+    def test__eq__(self, id_):
+        assert id_('proba') == id_('proba')
+        assert id_('proba') != id_('p')
 
-    def test__repr__(self):
-        assert repr(T_VTOKEN('proba')) == "T_VTOKEN('proba')"
+    def test__repr__(self, id_):
+        assert repr(id_('proba')) == "ID('proba')"
