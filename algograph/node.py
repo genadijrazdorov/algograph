2a25a7fbc9408240d7877dd708ad0ec931fb481d
Genadij Razdorov <genadijrazdorov@gmail.com>
2019-12-03 13:38:00 +0100

    Added __lt__ implementation to Node.

--- algograph/node.py
+++ algograph/node.py
@@ -17,6 +17,9 @@ class Node(abc.MutableMapping):
     def __eq__(self, other):
         return self.name == other.name
 
+    def __lt__(self, other):
+        return self.name < other.name
+
     def __delitem__(self, child):
         del self.children[child]
 
