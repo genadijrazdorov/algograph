cb747f3134b659b74e57e26cdea37f236df5e2a3
Genadij Razdorov <genadijrazdorov@gmail.com>
2019-12-16 13:39:15 +0100

    Separated general _run from graph specific run method in Graphviz

--- algograph/io/graphviz.py
+++ algograph/io/graphviz.py
@@ -3,32 +3,69 @@ from .dot import DOT
 import subprocess
 import sys
 import pathlib
-import shlex
 
-PATH_TO_GRAPHVIZ = ';'.join((
+
+GRAPHVIZ_PATH = (
+    # Graphviz direct install
     r'C:\Program Files (x86)\Graphviz2.38\bin',
-    r'C:\ProgramData\Anaconda3\Library\bin'
-))
+    # conda install
+    r'C:\ProgramData\Anaconda3\Library\bin\graphviz'
+)
 
 
 class Graphviz:
+    path = GRAPHVIZ_PATH
+
     # https://graphviz.gitlab.io/_pages/doc/info/output.html
-    def __init__(self, graph, format='svg', *args):
+    def __init__(self, graph=None, *args):
         self.graph = graph
+        self.args = args
 
-    def run(self, *args):
-        args = [shlex.quote(arg) for arg in args]
-        process = subprocess.run(
-            # FIXME: how to make it universal win/lin
-            ' '.join(['dot', '-Tcanon', *args]),
-            shell=True,
-            capture_output=True,
-            input=DOT(self.graph).encode(),
-            text=True,
-            check=False,
-            env=dict(PATH=PATH_TO_GRAPHVIZ) if sys.platform == 'win32' else None
-        )
-        return process
+    def dot(self):
+        if self.graph:
+            return DOT(self.graph).encode()
+        else:
+            return ''
 
-    def encode(self):
-        pass
+    def formats(self):
+        err = self._run('-T?').stderr.strip()
+        return err.split('Use one of: ', 1)[1].split()
+
+    def _run(self, *args, input=''):
+            if sys.platform == 'win32':
+                ext = '.exe'
+            else:
+                ext = ''
+
+            for path in ['.'] + list(self.path):
+                path = pathlib.Path(path)
+                prog = path.joinpath('dot').with_suffix(ext)
+                try:
+                    process = subprocess.run(
+                        [str(prog), *args],
+                        capture_output=True,
+                        text=True,
+                        input=input,
+                    )
+
+                except FileNotFoundError:
+                    continue
+
+                else:
+                    break
+
+            else:
+                raise FileNotFoundError('dot was not found on {}'.format(self.path))
+
+            return process
+
+    def run(self, format='svg'):
+        self.process = self._run('-T' + format, *self.args, input=self.dot())
+        if self.process.returncode:
+            if self.process.stderr.startswith('Format: '):
+                raise ValueError("Wrong '{}' format. Please use one of the: {}.".format(format, self.formats()))
+            raise ValueError(self.process.stderr.split('\nUsage: ', 1)[0])
+
+        return self.process.stdout
+
+
