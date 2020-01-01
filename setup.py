6a12b836ce642088b335ee4b5fb7f2ea7193eac4
Genadij Razdorov <genadijrazdorov@gmail.com>
2020-01-01 14:49:17 +0100

    Commented python versions not relevant

--- setup.py
+++ setup.py
@@ -115,13 +115,14 @@ setup(
         # that you indicate whether you support Python 2, Python 3 or both.
         # These classifiers are *not* checked by 'pip install'. See instead
         # 'python_requires' below.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python',
+        ## 'Programming Language :: Python :: 2',
+        ## 'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        ## 'Programming Language :: Python :: 3.5',
+        ## 'Programming Language :: Python :: 3.6',
+        ## 'Programming Language :: Python :: 3.7',
+        ## 'Programming Language :: Python :: 3.8',
     ],
 
     # This field adds keywords for your project which will appear on the
